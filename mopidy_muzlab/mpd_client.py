9d0b33869c6734136c7037855fe27b74e8a44955
as <a.sharf@muzis.ru>
2017-12-26 17:08:30 +0300

    change new_mpd_client

--- mopidy_muzlab/mpd_client.py
+++ mopidy_muzlab/mpd_client.py
@@ -2,18 +2,25 @@
 import socket
 from mpd import MPDClient
 from .repeating_timer import RepeatingTimer
-
+import time
 import logging
-logger = logging.getLogger(__name__)
-
 mpd_host = '127.0.0.1'
 mpd_port = 6600
 
 def new_mpd_client():
-        client = MPDClient()
-        client.timeout = 20
-        client.idletimeout = 20
-        client.connect(mpd_host, mpd_port)
-        return client
-        
+    client = MPDClient()
+    client.timeout = 20
+    client.idletimeout = 20
+    c = 0
+    while True:
+        try:
+            client.connect(mpd_host, mpd_port)
+            break
+        except Exception as es:
+            logging.warning(es)
+            c += 1
+            time.sleep(1)
 
+        if c >= 5:
+            break
+    return client
